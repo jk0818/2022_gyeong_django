321e791a7f7daff8457cac936c37ebbb8ed3fe64
jingyeong <jin980818@naver.com>
2022-05-12 18:29:49 +0900

    first

--- thirdapp/models.py
+++ thirdapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.fields import CharField, IntegerField
+from django.db.models.fields import CharField, IntegerField, FloatField, DateField
 
 class Shop(models.Model):
     shop_id = IntegerField(primary_key=True)
@@ -13,3 +13,69 @@ class Shop(models.Model):
         app_label = 'thirdapp'
         managed = False
 
+class JejuOlle(models.Model):
+    course = CharField(max_length=10)
+    course_name = CharField(max_length=20)
+    distance = FloatField()
+    time_info = CharField(max_length=10)
+    start_end_info = CharField(max_length=30)
+    cre_date = DateField()
+    class Meta:
+        db_table = 'jeju_olle'
+        managed = False
+
+class Owner(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    class Meta:
+        db_table = 'owner'
+class Animal(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    age = models.IntegerField(null=True)
+    owner = models.ForeignKey(Owner, on_delete=models.SET_NULL, null=True)
+    class Meta:
+        db_table = 'animal'
+
+class Warranty(models.Model):
+    model_nm = models.CharField(max_length=50, null=True)
+    period = models.IntegerField(null=True)
+    class Meta:
+        db_table = 'warranty'
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    price = models.IntegerField(null=True)
+    animal = models.ForeignKey(Animal, on_delete=models.SET_NULL, null=True)
+    warranty = models.OneToOneField(Warranty, on_delete=models.SET_NULL, null=True)
+    class Meta:
+        db_table = 'product'
+
+class Playground(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    address = models.CharField(max_length=50, null=True)
+    tel = models.CharField(max_length=20, null=True)
+    animals = models.ManyToManyField(Animal, null=True)
+    class Meta:
+        db_table = 'playground'
+
+
+
+class Dept(models.Model):
+    deptno = models.IntegerField(primary_key=True)
+    dname = models.CharField(max_length=14)
+    loc = models.CharField(max_length=13)
+    class Meta:
+        db_table = 'dept'
+        managed = False
+
+
+class Emp(models.Model):
+    empno = models.IntegerField(primary_key=True)
+    ename = models.CharField(max_length=10)
+    job = models.IntegerField(max_length=9)
+    mgr = models.IntegerField(null=True)
+    hiredate = models.DateField()
+    sal = models.IntegerField()
+    comm = models.IntegerField(null=True)
+    deptno = models.ForeignKey(Dept)
+    class Meta:
+        db_table = 'emp'
+        managed = False
--- thirdapp/views.py
+++ thirdapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Shop
+from .models import JejuOlle
+
 
 def shop(request):
     shop_list = Shop.objects.all()
@@ -7,4 +9,12 @@ def shop(request):
         request,
         'thirdapp/shop.html',
         {'shop_list': shop_list}
+    )
+
+def jeju_olle(request):
+    jeju_olle_list = JejuOlle.objects.all()
+    return render(
+        request,
+        'thirdapp/jeju_olle.html',
+        {'jeju_olle_list': jeju_olle_list}
     )
